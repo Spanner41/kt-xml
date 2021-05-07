a783c48c25bda5c6d930984b6b9a0af5976dc91a
Brady Steed <brady.steed@daveramsey.com>
2021-05-07 16:41:53 -0500

    wip: add ElementPrinter

---
+++ src/main/kotlin/design/cardia/ktxml/printer/ElementPrinter.kt
@@ -0,0 +1,19 @@
+package design.cardia.ktxml.printer
+
+import design.cardia.ktxml.builder.Element
+import design.cardia.ktxml.builder.XmlFormat
+import design.cardia.ktxml.builder.XmlVersion
+
+class ElementPrinter {
+  fun openTag(node: Element, format: XmlFormat, version: XmlVersion) =
+    "<${node.type}${node.attributesToXml(format, version)}>"
+
+  fun closeTag(node: Element) =
+    "</${node.type}>"
+
+  fun selfClosingTag(node: Element, format: XmlFormat, version: XmlVersion) =
+    "<${node.type}${node.attributesToXml(format, version)} />"
+
+  fun shouldCollapse(node: Element, format: XmlFormat) =
+    node.canCollapseWhenEmpty && format.collapseEmptyTags && node.children().isEmpty()
+}
